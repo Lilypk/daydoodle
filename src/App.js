40fe1e0722f8d8712ba3e05b11002cbf69436f7d
Lily Klopcic <lilyklopcic@gmail.com>
2020-12-16 18:20:22 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import './App.css';
-import axios from 'axios';
+//import axios from 'axios';
 import Homepage from './Homepage';
 import Feed from './Feed';
 import Canvas from './Canvas';
