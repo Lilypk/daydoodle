e9240faeb16cc6073348e714ddeb53c914fe4c51
Lily Klopcic <lilyklopcic@gmail.com>
2020-12-16 18:33:34 -0500

    Update Canvas.js

--- src/Canvas.js
+++ src/Canvas.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./Canvas.css";
 import ReactDOM from "react-dom";
 import CanvasDraw from "react-canvas-draw";
-import {Link} from 'react-router-dom'
+//import {Link} from 'react-router-dom'
 
 class Canvas extends Component {
   constructor(props) {
