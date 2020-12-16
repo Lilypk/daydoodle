484644c269cbb25827bf0f9daf0c807aa7fcfdeb
Lily Klopcic <lilyklopcic@gmail.com>
2020-12-16 18:19:49 -0500

    Update Feed.js

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./Feed.css";
-import {Link} from 'react-router-dom'
+//import {Link} from 'react-router-dom'
 import CanvasDraw from "react-canvas-draw";
 
 class Feed extends Component {
